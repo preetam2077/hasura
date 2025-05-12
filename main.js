25aa878ce4865027ca7c84f86b9725095605dd5c
preetam2077 <78674551+preetam2077@users.noreply.github.com>
2025-05-12 16:13:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,100 @@
+const spinbtn = document.getElementById("mainbtn")
+let balance = 1000
+let bal = document.getElementById("bal")
+bal.textContent = "$"+String(balance)
+let bdown = false       
+
+const nodivs = document.getElementsByClassName("nodiv")
+
+const n1 = document.getElementById("n1")
+const n2 = document.getElementById("n2")
+const n3 = document.getElementById("n3")
+
+let canCall = false
+
+let inp = document.getElementById("inp")
+
+
+inp.addEventListener('keyup', goodInp)
+
+function goodInp(){
+    print(Boolean(inp.value))
+    if(String(inp.value).includes(".")){
+        console.log("Invalidddd")
+        let betinp = document.getElementById("betinp")
+        betinp.style.backgroundImage = "linear-gradient(#AA3333, #FFFFFF, #AA3333)"
+    }
+    else{
+        document.getElementById("betinp").style.backgroundImage = "linear-gradient(#FFAC13, #FFFFFF, #FFAC13)"
+    }
+}
+
+function balUp(){
+    bal.textContent = "$" + String(balance)
+    console.log("updated")
+    bdown = false
+}
+
+function didwin(){
+        if(nodivs[0].childNodes[0].textContent === nodivs[1].childNodes[0].textContent === nodivs[2].childNodes[0].textContent){
+            console.log("Won!")
+            spinbtn.classList.remove("spinbtn-active")
+            // balance += 50
+            balUp()
+            
+        }else{
+            console.log("Lost!")
+            spinbtn.classList.remove("spinbtn-active")
+            // balance -= 50
+            balUp()
+        }
+}
+
+function spun(){
+    console.log("clicked")
+    if(bdown == false){
+        bdown = true
+        console.log(bdown)
+        spinbtn.classList.add("spinbtn-active")
+
+        let betAmount = inp.value
+        if (betAmount == 0 || String(betAmount)== ''){
+            balance-=50
+        }else{
+            balance -= betAmount
+        }
+
+
+        
+        for(let nodiv of nodivs){
+            nodiv.childNodes[0].style.animationName = "slidedown"
+            nodiv.childNodes[0].style.animationDuration = "0.5s"
+            nodiv.childNodes[0].style.animationTimingFunction = "ease-in-out"
+            // nodiv.childNodes[0].textContent = Math.floor(Math.random()*8)
+        }
+
+        inp.style.animationName = "shine"
+        inp.style.animationDuration = "0.5s"
+        inp.style.animationTimingFunction = "ease-in-out"
+
+        setTimeout(function(){
+            for(let nodiv of nodivs){
+            nodiv.childNodes[0].style.animationName = "slideup"
+            nodiv.childNodes[0].style.animationDuration = "0.5s"
+            nodiv.childNodes[0].style.animationTimingFunction = "ease-in-out"
+            nodiv.childNodes[0].textContent = Math.floor(Math.random()*7)+1
+            inp.style.animationName = "none"
+            canCall=true
+        }}, 350)
+        
+        setTimeout(function(){
+            if(canCall){didwin()}
+            canCall= false
+        }, 3000)
+    }
+    
+}
+
+
+
+spinbtn.addEventListener("click", spun)
