282bd506017d89c12fc7a32a4117094439ecf280
preetam2077 <78674551+preetam2077@users.noreply.github.com>
2025-05-13 00:11:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,49 +11,67 @@ const n2 = document.getElementById("n2")
 const n3 = document.getElementById("n3")
 
 let canCall = false
+let isGoodInput = true;
 
 let inp = document.getElementById("inp")
 
 
 inp.addEventListener('keyup', goodInp)
 
+let betAmount = inp.value
 
-
-
+function goodInp(){
+    if(String(inp.value).includes(".")){
+        console.log("Invalidddd")
+        inp.style.backgroundColor = "red";
+        isGoodInput = false
+    }
+    else{
+        inp.style.backgroundColor = "#252525";
+        console.log("valid")
+        isGoodInput = true
+    }
+}
 
 function balUp(){
     bal.textContent = "$" + String(balance)
+    bal.style.fontSize = `${32-bal.textContent.length}px`
     console.log("updated")
     bdown = false
 }
 
-function didwin(){
-        if(nodivs[0].childNodes[0].textContent === nodivs[1].childNodes[0].textContent === nodivs[2].childNodes[0].textContent){
-            console.log("Won!")
+function didwin(bet){
+        if(nodivs[0].childNodes[0].textContent === nodivs[1].childNodes[0].textContent && nodivs[0].childNodes[0].textContent ===  nodivs[2].childNodes[0].textContent){
+            console.log(bet)
             spinbtn.classList.remove("spinbtn-active")
-            // balance += 50
+            balance += bet
             balUp()
+            console.log("you won")
             
         }else{
-            console.log("Lost!")
+            console.log(bet)
             spinbtn.classList.remove("spinbtn-active")
-            // balance -= 50
+            balance -= bet
             balUp()
+            console.log("you lost")
         }
 }
 
 function spun(){
     console.log("clicked")
-    if(bdown == false){
+    let betAmount = inp.value
+    if(bdown == false && isGoodInput  && betAmount <= balance && balance > 0){
         bdown = true
         console.log(bdown)
         spinbtn.classList.add("spinbtn-active")
 
-        let betAmount = inp.value
-        if (betAmount == 0 || String(betAmount)== ''){
-            balance-=50
+        
+        if (betAmount == 0 || String(betAmount)== '' ) {
+            betAmount = Number(betAmount)
+            console.log(typeof betAmount)
+            betAmount = 50
         }else{
-            balance -= betAmount
+            betAmount = betAmount
         }
 
 
@@ -65,6 +83,24 @@ function spun(){
             // nodiv.childNodes[0].textContent = Math.floor(Math.random()*8)
         }
 
+        setTimeout(function (){
+            let n1 = Math.floor(Math.random()*7)+1
+            let n2
+            let n3
+            let weight = Math.random()
+            if (weight <0.5){
+                console.log("weight is", weight)
+                n2 = n1;
+                n3 = n1;
+            }else{
+                n2 = Math.floor(Math.random()*7)+1
+                n3 = Math.floor(Math.random()*7)+1
+            }
+            nodivs[0].childNodes[0].textContent = n1
+            nodivs[1].childNodes[0].textContent = n2
+            nodivs[2].childNodes[0].textContent = n3
+        }, 250)
+
         inp.style.animationName = "shine"
         inp.style.animationDuration = "0.5s"
         inp.style.animationTimingFunction = "ease-in-out"
@@ -74,14 +110,18 @@ function spun(){
             nodiv.childNodes[0].style.animationName = "slideup"
             nodiv.childNodes[0].style.animationDuration = "0.5s"
             nodiv.childNodes[0].style.animationTimingFunction = "ease-in-out"
-            nodiv.childNodes[0].textContent = Math.floor(Math.random()*7)+1
+            
+            // nodiv.childNodes[0].textContent = Math.floor(Math.random()*7)+1
             inp.style.animationName = "none"
             canCall=true
-        }}, 350)
+            }
+        }, 350)
+        
         
         setTimeout(function(){
-            if(canCall){didwin()}
-            canCall= false
+            if(canCall){didwin(betAmount)}
+            canCall= false 
+            
         }, 3000)
     }
     
